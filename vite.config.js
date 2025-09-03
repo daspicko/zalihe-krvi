158deb1b2331083fbd07950bfd355424ac890cdd
Davorin Špičko <davorin.spicko@gmail.com>
2025-09-03 13:28:18 +0200

    WIP: Notifications

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   baseToReplace: '',
-  plugins: [react()],
+  base: '/zalihe-krvi',
+  plugins: [
+    react()
+  ],
+  server: {
+    allowedHosts: ['localhost', '10eabcf7bf40.ngrok-free.app']
+  }
 })
