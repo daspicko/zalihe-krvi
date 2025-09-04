69e48fcca884302ce58dbf5ca664f5e614cb7f9e
Davorin Špičko <davorin.spicko@gmail.com>
2025-09-04 20:36:44 +0200

    [Feature] Fix worker config. (#9)

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
 
-import firebaseConfig from './js/config.js';
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB2Tk7wEM_XgNY_tqXqtZClpgNr4rk_Fz8",
+  authDomain: "zalihe-krvi.firebaseapp.com",
+  projectId: "zalihe-krvi",
+  storageBucket: "zalihe-krvi.firebasestorage.app",
+  messagingSenderId: "1050380975409",
+  appId: "1:1050380975409:web:59b2f2089821b94ff54410",
+  measurementId: "G-Z3CCKNPS7N"
+};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
 const messaging = firebase.messaging(firebaseApp);
